2626f086df6d0f84acf4e48732363e3a312fbb25
Dan Ngo <dan.ngo.tech@gmail.com>
2026-07-13 15:57:48 -0400

    chore: use lf newlines

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,14 +1,14 @@
-import path from "path";
-import { run } from "../core/runtime.js";
-
-export function cli(argv) {
-  const entry = argv[2];
-
-  if (!entry) {
-    console.error("Usage: hot <entry-file>");
-    process.exit(1);
-  }
-
-  const abs = path.resolve(entry);
-  run(abs);
-}
+import path from "path";
+import { run } from "../core/runtime.js";
+
+export function cli(argv) {
+  const entry = argv[2];
+
+  if (!entry) {
+    console.error("Usage: hot <entry-file>");
+    process.exit(1);
+  }
+
+    const abs = path.resolve(entry);
+  run(abs);
+}
